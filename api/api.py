ecda5e7133e8b952f029be8ae31b2cc2bcef7cc5
Bora Akyuz <bora.akyuz@warwick.ac.uk>
2024-04-30 17:19:47 +0100

    add simple api

---
+++ api/api.py
@@ -0,0 +1,71 @@
+import sqlite3
+from pydantic import BaseModel
+from fastapi.responses import Response
+from typing import List
+
+from fastapi import (
+    FastAPI,
+    Query,
+    Depends,
+    HTTPException,
+)
+from fastapi.security import HTTPBasic
+from fastapi.middleware.cors import CORSMiddleware
+
+
+class WordDefinitionModel(BaseModel):
+    type: str
+    definitions: List[str]
+
+
+app = FastAPI()
+
+# Allow requests from these origins
+origins = [
+    "https://api.boraakyuz.me",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/search_word")
+def search_word(word: str = Query(..., min_length=2)):
+    conn = sqlite3.connect("tdk12.db")
+    cursor = conn.cursor()
+
+    query = "SELECT word FROM dictionary WHERE word LIKE ? LIMIT 5"
+
+    search_term = f"{word}%"
+
+    cursor.execute(query, (search_term,))
+    results = cursor.fetchall()
+
+    matching_words = [row[0] for row in results]
+
+    cursor.close()
+    conn.close()
+    return {"matching_words": matching_words}
+
+
+@app.get("/get_definitions/{word}")
+def get_definitions(word: str):
+    conn = sqlite3.connect("tdk12.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT meanings FROM dictionary WHERE UPPER(word) = UPPER(?)", (word,))
+    result = cursor.fetchone()
+
+    if not result:
+        raise HTTPException(status_code=400, detail="Definition not found!")
+
+    cursor.close()
+    conn.close()
+    meanings = result[0]
+    return {"definitions": meanings}
+        
